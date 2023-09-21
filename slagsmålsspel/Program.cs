b46e13268a6362a84e9cb32f0449169a22a9f322
Liam Esseen <Liam.esseen@elev.ga.ntig.se>
2023-09-21 10:56:54 +0200

    slump och while loop

---
+++ slagsmålsspel/Program.cs
@@ -0,0 +1,24 @@
+﻿//SLUMP
+
+//Random generator = new Random();
+
+//int slumptal = generator.Next(10, 20);
+
+//Console.WriteLine(slumptal);
+
+
+
+//WHITE-LOOP
+
+int tal = 100;
+
+while (tal > 0)
+{
+Console.WriteLine(tal);
+tal--;
+}
+
+Console.WriteLine("FÄRDIG");
+
+
+Console.ReadLine();
